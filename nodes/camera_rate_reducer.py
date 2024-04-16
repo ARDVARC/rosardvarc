226d2e590dd9ed341f3dacf057683a075b59639e
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2024-04-15 18:00:43 -0600

    Make rate reducer slightly smarter

--- nodes/camera_rate_reducer.py
+++ nodes/camera_rate_reducer.py
@@ -3,14 +3,15 @@ from FSW.config.topic_names import CAMERA_FRAMES, BAGGED_FRAMES
 from sensor_msgs.msg import Image
 
 
-_previous_frame_time: float = 0
+_previous_frame_time: float = rospy.Time.now().to_sec()
 _desired_rate: float = 5 # Hz
+_DESIRED_FRAME_GAP = 1/_desired_rate
 
 def frame_callback(msg: Image):
     global _previous_frame_time
-    if msg.header.stamp.to_sec() - 1/_desired_rate > _previous_frame_time:
+    if msg.header.stamp.to_sec() - _DESIRED_FRAME_GAP > _previous_frame_time:
         slow_frame_pub.publish(msg)
-        _previous_frame_time = msg.header.stamp.to_sec()
+        _previous_frame_time += _DESIRED_FRAME_GAP
 
 
 rospy.init_node("camera_rate_reducer")
