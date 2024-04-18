8aad09ef2215ba7fd239bed539b4cd701dbb0cd6
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2024-04-18 16:53:28 -0600

    Update launch script to include rosbag

--- scripts/tmux_layout.sh
+++ scripts/tmux_layout.sh
@@ -9,7 +9,7 @@ sleep 3
 # Open new tmux session. In first window, prepare to run the main FSW launch script
 tmux -2 new-session -d -x "$(tput cols)" -y "$(tput lines)" -s "GCS" -n "FSW" 'echo "REMEMBER TO FIX LOCAL ORIGIN, UPDATE MAVROS RATES, AND RUN ROSBAG!"; bash'
 sleep 2
-tmux send-keys "roslaunch rosardvarc fsw_and_io.launch"
+tmux send-keys "roslaunch rosardvarc fsw_and_io.launch bag_name:=CHANGE_THIS"
 
 # In second window, add loggers for
 # - Mission state
@@ -28,13 +28,8 @@ tmux split-window -t "GCS:1" -v -p 50 -d 'rostopic echo /bluetooth/az_els -c -w 
 tmux select-pane  -t "GCS:1.{right}"
 tmux split-window -t "GCS:1" -v -p 50 -d 'rostopic echo /mavros/local_position/pose -c -w 5'
 
-# In third window, add rosbag command
-tmux new-window -t "GCS:2" -n 'ROSBAG'
-sleep 4
-tmux send-keys "rosbag record -o ~/rosbags/ -b 0 --split --size=1024 --chunksize=128 bluetooth/az_els mavros/local_position/pose estimation/direction_vectors_uas estimation/estimated_rgv_states state_machine/mission_states state_machine/state_machine_criteria state_machine/forced usb_cam/image_raw/compressed estimation/rgv_local_projections camera/recent_rgv_sightings mavros/state"
-
-# Add empty fourth window for commands
-tmux new-window -t "GCS:3" -n 'COMMANDS'
+# Add empty third window for commands
+tmux new-window -t "GCS:2" -n 'COMMANDS'
 
 # Attach to session
 tmux select-window -t "GCS:0"
